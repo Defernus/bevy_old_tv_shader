2a0acf6c6c9abf7bee88f372130add6a38d8a1e0
Defernus <dehucka98@gmail.com>
2022-08-04 18:29:37 +0300

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple "old tv" looking effect
+based on bevy [post processing example](https://github.com/bevyengine/bevy/blob/main/examples/shader/post_processing.rs)
+
+![alt screenshot](/screenshot.png)
+
---
+++ src/main.rs
@@ -0,0 +1,179 @@
+use bevy::{
+    core_pipeline::clear_color::ClearColorConfig,
+    prelude::*,
+    reflect::TypeUuid,
+    render::{
+        camera::{Camera, RenderTarget},
+        render_resource::{
+            AsBindGroup, Extent3d, ShaderRef, TextureDescriptor, TextureDimension, TextureFormat,
+            TextureUsages,
+        },
+        view::RenderLayers,
+    },
+    sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle},
+};
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins)
+        .add_plugin(Material2dPlugin::<PostProcessingMaterial>::default())
+        .add_startup_system(setup)
+        .add_system(main_camera_cube_rotator_system);
+
+    app.run();
+}
+
+#[derive(Component)]
+struct MainCube;
+
+fn setup(
+    mut commands: Commands,
+    mut windows: ResMut<Windows>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut post_processing_materials: ResMut<Assets<PostProcessingMaterial>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut images: ResMut<Assets<Image>>,
+    asset_server: Res<AssetServer>,
+) {
+    asset_server.watch_for_changes().unwrap();
+
+    let window = windows.get_primary_mut().unwrap();
+    let size = Extent3d {
+        width: window.physical_width(),
+        height: window.physical_height(),
+        ..default()
+    };
+
+    let mut image = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::Bgra8UnormSrgb,
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+                | TextureUsages::RENDER_ATTACHMENT,
+        },
+        ..default()
+    };
+
+    image.resize(size);
+
+    let image_handle = images.add(image);
+
+    let cube_handle = meshes.add(Mesh::from(shape::Cube { size: 4.0 }));
+    let cube_material_handle = materials.add(StandardMaterial {
+        base_color: Color::rgb(0.8, 0.7, 0.6),
+        reflectance: 0.02,
+        unlit: false,
+        ..default()
+    });
+
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: cube_handle,
+            material: cube_material_handle,
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 1.0)),
+            ..default()
+        })
+        .insert(MainCube);
+
+    commands.spawn_bundle(PointLightBundle {
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 10.0)),
+        ..default()
+    });
+
+    commands.spawn_bundle(Camera3dBundle {
+        camera_3d: Camera3d {
+            clear_color: ClearColorConfig::Custom(Color::WHITE),
+            ..default()
+        },
+        camera: Camera {
+            target: RenderTarget::Image(image_handle.clone()),
+            ..default()
+        },
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 15.0))
+            .looking_at(Vec3::default(), Vec3::Y),
+        ..default()
+    });
+
+    let post_processing_pass_layer = RenderLayers::layer((RenderLayers::TOTAL_LAYERS - 1) as u8);
+
+    let quad_handle = meshes.add(Mesh::from(shape::Quad::new(Vec2::new(
+        size.width as f32,
+        size.height as f32,
+    ))));
+
+    let material_handle = post_processing_materials.add(PostProcessingMaterial {
+        screen_shape_factor: 0.2,
+        rows: 128.0,
+        brightness: 3.0,
+        edges_transition_size: 0.025,
+        channels_mask_min: 0.1,
+        source_image: image_handle,
+    });
+
+    commands
+        .spawn_bundle(MaterialMesh2dBundle {
+            mesh: quad_handle.into(),
+            material: material_handle,
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 1.5),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(post_processing_pass_layer);
+
+    commands
+        .spawn_bundle(Camera2dBundle {
+            camera: Camera {
+                priority: 1,
+                ..default()
+            },
+            ..Camera2dBundle::default()
+        })
+        .insert(post_processing_pass_layer);
+}
+
+fn main_camera_cube_rotator_system(
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<MainCube>>,
+) {
+    for mut transform in &mut query {
+        transform.rotate_x(0.55 * time.delta_seconds());
+        transform.rotate_z(0.15 * time.delta_seconds());
+    }
+}
+
+#[derive(AsBindGroup, TypeUuid, Clone)]
+#[uuid = "b17e3ec0-b8e2-4b66-a62e-1ed9f4374350"]
+struct PostProcessingMaterial {
+    /// value between 0 and 1. grater value -> more round shape
+    #[uniform(0)]
+    screen_shape_factor: f32,
+
+    #[uniform(1)]
+    rows: f32,
+
+    #[uniform(2)]
+    brightness: f32,
+
+    #[uniform(3)]
+    edges_transition_size: f32,
+
+    #[uniform(4)]
+    channels_mask_min: f32,
+
+    #[texture(5)]
+    #[sampler(6)]
+    source_image: Handle<Image>,
+}
+
+impl Material2d for PostProcessingMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "post_processing.wgsl".into()
+    }
+}
